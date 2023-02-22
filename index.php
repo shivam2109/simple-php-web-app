d650a1ed2c68064e0988ba7e3089593894ea3571
Shivam Sahu <69560240+shivam2109@users.noreply.github.com>
2023-02-22 19:08:49 +0000

    Added index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title> <?php echo "Simple Maths" ?> </title>
+</head>
+  <body>
+    <?php
+    $a = 81;
+    $b = 9;
+    echo "<p>Provided values of a and b are ".$a." and ".$b." respectively.<br>Simple Maths are as follows:<br></p>";
+    echo "a + b = ".($a + $b)."<br>";
+    echo "a - b = ".($a - $b)."<br>";
+    echo "a x b = ".($a * $b)."<br>";
+    echo "a / b = ".($a / $b)."<br>";
+    echo "a % b = ".($a % $b)."<br>";
+    ?>
+</body>
+</html>
